6ccd9fcb3727e7a7e0a082efeec999fa53dcd9a7
werka <werma56s@gmail.com>
2020-05-29 20:52:04 +0200

    P_APP

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace AplikacjaFizycznaElektrostatyka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2Y = new System.Windows.Forms.Label();
+            this.label1X = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 285);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Czyść";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(0, 213);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Linie siły pola";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightGray;
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Location = new System.Drawing.Point(9, 102);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(162, 90);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dodaj ładunek";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 20);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ujemne";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 20);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "dodatenie";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 366);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightGray;
+            this.groupBox2.Controls.Add(this.label2Y);
+            this.groupBox2.Controls.Add(this.label1X);
+            this.groupBox2.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox2.Location = new System.Drawing.Point(9, 10);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(162, 73);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Współrzedne";
+            // 
+            // label2Y
+            // 
+            this.label2Y.AutoSize = true;
+            this.label2Y.Location = new System.Drawing.Point(83, 36);
+            this.label2Y.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2Y.Name = "label2Y";
+            this.label2Y.Size = new System.Drawing.Size(26, 16);
+            this.label2Y.TabIndex = 1;
+            this.label2Y.Text = "Y=";
+            // 
+            // label1X
+            // 
+            this.label1X.AutoSize = true;
+            this.label1X.Location = new System.Drawing.Point(13, 36);
+            this.label1X.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1X.Name = "label1X";
+            this.label1X.Size = new System.Drawing.Size(26, 16);
+            this.label1X.TabIndex = 0;
+            this.label1X.Text = "X=";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(175, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(327, 366);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Window;
+            this.panel4.Location = new System.Drawing.Point(13, 46);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(300, 300);
+            this.panel4.TabIndex = 6;
+            this.panel4.Click += new System.EventHandler(this.panel4_Click);
+            this.panel4.MouseLeave += new System.EventHandler(this.panel4_MouseLeave);
+            this.panel4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseMove);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Controls.Add(this.dateTimePicker1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(327, 23);
+            this.panel3.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.DarkGray;
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dateTimePicker1.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(36, 2);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(251, 19);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 366);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Program pokazujacy linie siły pola 2 ładnków (+) i 2 ładunków (-)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2Y;
+        private System.Windows.Forms.Label label1X;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacjaFizycznaElektrostatyka
+{
+    public partial class Form1 : Form
+    {
+        Graphics g; Pen p; Point cursor;
+        int k = 0, n = 0; Point[] punkty_czerwone = new Point[2];
+        Point[] punkty_niebieskie = new Point[2];
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void panel4_MouseMove(object sender, MouseEventArgs e)
+        {
+            cursor = new Point(e.X, e.Y);
+            label1X.Text = "X= " + e.X;
+            label2Y.Text = "Y= " + e.Y;
+        }
+        private void panel4_MouseLeave(object sender, EventArgs e)
+        {
+            label1X.Text = "X= ";
+            label2Y.Text = "Y= ";
+        }
+       
+        int scale = 15;
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            //kod tworzacy uklad wspolrzednych
+            e.Graphics.DrawLine(Pens.BlueViolet, panel4.Width / 2, 0, panel4.Width / 2, panel4.Height);
+            e.Graphics.DrawLine(Pens.BlueViolet, 0, panel4.Height / 2, panel4.Width, panel4.Height / 2);
+            Point absZero = new Point(panel4.Width / 2, panel4.Height / 2);
+            int i = absZero.X;
+            while (i < panel4.Width)
+            {
+                i += scale;
+                e.Graphics.DrawLine(Pens.BlueViolet, i, absZero.Y - 4, i, absZero.Y + 4);
+            }
+            e.Graphics.DrawLine(Pens.BlueViolet, absZero.X, 0, absZero.X, panel4.Height);
+            e.Graphics.DrawLine(Pens.BlueViolet, 0, absZero.Y, panel4.Width, absZero.Y);
+
+            while (i < panel4.Width)
+            {
+                i += scale;
+                e.Graphics.DrawLine(Pens.BlueViolet, i, absZero.Y - 4, i, absZero.Y + 4);
+            }
+            i = absZero.X;
+            while (i > 0)
+            {
+                i -= scale;
+                e.Graphics.DrawLine(Pens.BlueViolet, i, absZero.Y - 4, i, absZero.Y + 4);
+            }
+            i = absZero.Y;
+            while (i < panel4.Height)
+            {
+                i += scale;
+                e.Graphics.DrawLine(Pens.BlueViolet, absZero.X - 4, i, absZero.X + 4, i);
+            }
+            i = absZero.Y;
+            while (i > 0)
+            {
+                i -= scale;
+                e.Graphics.DrawLine(Pens.BlueViolet, absZero.X - 4, i, absZero.X + 4, i);
+            }
+
+            /*int X = 0;
+            int Y = 0;
+            int count = 10;
+            
+                for (int k = -10; k <= count; k++)
+                {
+                    X = k;
+                    Y = 2 * X;
+                    e.Graphics.FillEllipse(Brushes.DarkGreen, (absZero.X + (X * scale)) - 2,
+                    (absZero.Y - (Y * scale)) - 2, 4, 4);
+                }*/
+        }
+
+        Point point1, point2, point1a, point2a;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            k = 0; n = 0;
+            Array.Clear(punkty_czerwone, 0, k);
+            Array.Clear(punkty_niebieskie, 0, n);
+            g.Clear(Color.White);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g = panel4.CreateGraphics();
+
+            Pen blackPen = new Pen(Color.Black, 1);
+            Pen blackPen1 = new Pen(Color.Red, 1);
+            Pen blackPen2 = new Pen(Color.Blue, 1);
+
+            if (k == 2 && n == 2)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    point1 = punkty_czerwone[i]; i++;
+                    point1a = punkty_czerwone[i];
+                    g.DrawLine(blackPen1, point1, point1a);
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        point2 = punkty_niebieskie[j]; j++;
+                        point2a = punkty_niebieskie[j];
+
+                        g.DrawLine(blackPen, point1, point2);
+                        g.DrawLine(blackPen, point1a, point2);
+
+
+                        g.DrawLine(blackPen2, point2, point2a);
+
+                        g.DrawLine(blackPen, point2a, point1);
+                        g.DrawLine(blackPen, point2a, point1a);
+
+                    }
+
+                }
+            }
+            else if (n == 2 && k == 0)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    point2 = punkty_niebieskie[j]; j++;
+                    point2a = punkty_niebieskie[j];
+
+                    g.DrawLine(blackPen2, point2, point2a);
+                }
+            }
+            else if (n == 0 && k == 2)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    point2 = punkty_czerwone[i]; i++;
+                    point2a = punkty_czerwone[i];
+
+                    g.DrawLine(blackPen1, point2, point2a);
+                }
+            }
+            else if (k == 1 && n==2)
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    point1 = punkty_czerwone[i];  
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        point2 = punkty_niebieskie[j]; j++;
+                        point2a = punkty_niebieskie[j];
+
+                        g.DrawLine(blackPen, point1, point2);
+
+                        g.DrawLine(blackPen2, point2, point2a);
+
+                        g.DrawLine(blackPen, point2a, point1);
+                    }
+                }
+            }
+            else if (n == 1 && k == 2) 
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    point1 = punkty_niebieskie[i];
+
+                    for (int j = 0; j < k; j++)
+                    {
+                        point2 = punkty_czerwone[j]; j++;
+                        point2a = punkty_czerwone[j];
+
+                        g.DrawLine(blackPen, point1, point2);
+
+                        g.DrawLine(blackPen1, point2, point2a);
+
+                        g.DrawLine(blackPen, point2a, point1);
+                    }
+                }
+            }
+            else if (n == 1 && k == 1)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    point1 = punkty_niebieskie[i];
+
+                    for (int j = 0; j < k; j++)
+                    {
+                        point2 = punkty_czerwone[j]; 
+
+                        g.DrawLine(blackPen, point1, point2);
+
+                        g.DrawLine(blackPen, point2, point1);
+                    }
+                }
+            }
+
+        }
+
+        private void panel4_Click(object sender, EventArgs e)
+        {
+
+            g = panel4.CreateGraphics();
+            p = new Pen(Color.Red, 20);
+
+            if (radioButton1.Checked == true && radioButton2.Checked == false)
+            {
+                if (k < 2)
+                {
+                    g.DrawEllipse(p, cursor.X - 10, cursor.Y - 10, 20, 20);
+                    punkty_czerwone[k++] = new Point(cursor.X, cursor.Y);
+                }
+                else
+                {
+                    MessageBox.Show("Możesz uzyć maksymalnie 2 ładunków dodoatnich!", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            else if (radioButton2.Checked == true && radioButton1.Checked == false)
+            {
+                if (n < 2)
+                {
+                    p = new Pen(Color.Blue, 20);
+                    g.DrawEllipse(p, cursor.X - 10, cursor.Y - 10, 20, 20);
+                    punkty_niebieskie[n++] = new Point(cursor.X, cursor.Y);
+                }
+                else
+                {
+                    MessageBox.Show("Możesz uzyć maksymalnie 2 ładunków ujemnych!", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+
+
+        }
+
+    }
+
+}
+
